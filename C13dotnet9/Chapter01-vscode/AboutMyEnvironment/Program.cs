45330d354a1527d5eb542561dc83eb0a340a1d0f
coleman07 <coleman07@gmail.com>
2024-11-23 11:20:14 -0700

    vscode version of chapter 01

---
+++ C13dotnet9/Chapter01-vscode/AboutMyEnvironment/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace AboutMyEnvironment;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // See https://aka.ms/new-console-template for more information
+        Console.WriteLine(Environment.CurrentDirectory);
+        Console.WriteLine(Environment.OSVersion.VersionString);
+        Console.WriteLine("Namespace: {0}", typeof(Program).Namespace);
+    }
+}
