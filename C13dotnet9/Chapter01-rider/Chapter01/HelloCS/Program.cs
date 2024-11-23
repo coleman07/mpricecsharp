9b807815e36c02b8c25c0f7fd06402e8ad03b735
coleman07 <coleman07@gmail.com>
2024-11-23 11:14:11 -0700

    initial commit

---
+++ C13dotnet9/Chapter01-rider/Chapter01/HelloCS/Program.cs
@@ -0,0 +1,4 @@
+﻿        string name = typeof(Program).Namespace ?? "None!";
+        Console.WriteLine("Hello, C#!");
+        Console.WriteLine($"Namespace: {name}");
+        throw new Exception();
